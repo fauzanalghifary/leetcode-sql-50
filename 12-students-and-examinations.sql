4f4731ea8d46fd60ec94a1c802a53450597fae91
fauzanalghifary <m.fauzanghifary@gmail.com>
2026-06-24 21:48:38 +0700

    finish 12

---
+++ 12-students-and-examinations.sql
@@ -0,0 +1,7 @@
+SELECT st.student_id, st.student_name, su.subject_name, COUNT(e.subject_name) as attended_exams
+FROM students st
+CROSS JOIN subjects su
+LEFT JOIN examinations e
+    ON st.student_id = e.student_id AND su.subject_name = e.subject_name
+GROUP BY st.student_id, st.student_name, su.subject_name
+ORDER BY st.student_id, su.subject_name
