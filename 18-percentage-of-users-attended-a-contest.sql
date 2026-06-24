6c47d3e6a251162503d46a605ae0b00a1c743f71
fauzanalghifary <m.fauzanghifary@gmail.com>
2026-06-24 23:13:45 +0700

    finish 18

---
+++ 18-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,6 @@
+SELECT
+    r.contest_id,
+    ROUND(COUNT(DISTINCT r.user_id) / (SELECT COUNT(*) FROM users) * 100, 2) AS percentage
+FROM register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id;
