1c54f8b11dc4a8c79e5cefc82c7510acac85694c
fauzanalghifary <m.fauzanghifary@gmail.com>
2026-06-24 22:06:22 +0700

    finish 13

--- 13-managers-with-at-least-5-direct-report.sql
+++ 13-managers-with-at-least-5-direct-report.sql
@@ -6,3 +6,10 @@ JOIN (
 ) m1 ON e1.managerId = m1.id
 GROUP BY m1.id
 HAVING COUNT(m1.id) >= 5
+
+
+SELECT a.name 
+FROM Employee a 
+JOIN Employee b ON a.id = b.managerId 
+GROUP BY b.managerId 
+HAVING COUNT(*) >= 5
