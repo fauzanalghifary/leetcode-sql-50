fe0dee1255707fa57b139a7551b181f77f6ebade
fauzanalghifary <m.fauzanghifary@gmail.com>
2026-06-29 08:06:00 +0700

    finish 34

---
+++ 34-product-price-at-a-given-date.sql
@@ -0,0 +1,15 @@
+SELECT product_id, new_price AS price
+FROM Products p1
+WHERE change_date = (
+    SELECT MAX(change_date)
+    FROM Products p2
+    WHERE p2.product_id = p1.product_id
+      AND p2.change_date <= '2019-08-16'
+)
+
+UNION
+
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16';
